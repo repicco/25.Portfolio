7a8d2d5a50f59c352857644614182eaf8bb98b24
Renato Picco <56326785+repicco@users.noreply.github.com>
2020-04-15 03:12:13 -0300

    Deploy.13
    Add. pasta Prod

---
+++ portfolio/prod/precache-manifest.bdcf1cba5e970b8ec5597d467cb0aa22.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ddc681fe74944afffe5007c4b2b97b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2b9756934bb67f7a80d",
+    "url": "/static/css/main.c2c2b78f.chunk.css"
+  },
+  {
+    "revision": "e495d4d641f41fb79513",
+    "url": "/static/js/2.e840e7ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e840e7ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b9756934bb67f7a80d",
+    "url": "/static/js/main.16615fdf.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "11880156aac0e0457e4bfc30a983e39e",
+    "url": "/static/media/Back.11880156.png"
+  },
+  {
+    "revision": "c032fc0800b3fd81e496d814302fb52c",
+    "url": "/static/media/Renato.c032fc08.jpg"
+  },
+  {
+    "revision": "d98a919d4f89c3d3f8b99929cf44d924",
+    "url": "/static/media/airbnb.d98a919d.jpg"
+  },
+  {
+    "revision": "4d9c59d57be54f8b6e05773ec2326441",
+    "url": "/static/media/atendi.4d9c59d5.jpg"
+  },
+  {
+    "revision": "4f759f83b90604a842d3ce60d9329ec9",
+    "url": "/static/media/basico.4f759f83.jpg"
+  },
+  {
+    "revision": "76f60f25ffecd6f30449d5d612dee40c",
+    "url": "/static/media/biscoito.76f60f25.jpg"
+  },
+  {
+    "revision": "8b50d1862015dbb9ba371a0364756c45",
+    "url": "/static/media/bonus.8b50d186.jpg"
+  },
+  {
+    "revision": "c7ef1d9523b5921eb1fa07adda0d4444",
+    "url": "/static/media/churras.c7ef1d95.jpg"
+  },
+  {
+    "revision": "1c430552a32664989f60b9219fa1f6cf",
+    "url": "/static/media/financas.1c430552.jpg"
+  },
+  {
+    "revision": "557f620be6b666cbfb192ad8fa3eeefc",
+    "url": "/static/media/mercado.557f620b.jpg"
+  },
+  {
+    "revision": "c624cb04bb24827a17eccdd4a0cb528e",
+    "url": "/static/media/museu.c624cb04.jpg"
+  },
+  {
+    "revision": "006372ce2c6100f4af00e9520782f41e",
+    "url": "/static/media/orcamento.006372ce.jpg"
+  },
+  {
+    "revision": "8f54895b22d87f303e1e92b64d1753bb",
+    "url": "/static/media/parallax.8f54895b.jpg"
+  },
+  {
+    "revision": "0730877c487e1e2de7ecab02ecc321a4",
+    "url": "/static/media/poupedinheiro.0730877c.jpg"
+  },
+  {
+    "revision": "8a9c61eda840fe12204d82bb231bec28",
+    "url": "/static/media/profile.8a9c61ed.jpg"
+  },
+  {
+    "revision": "f8c9b0f077303415fcc559639d8e0050",
+    "url": "/static/media/spotify.f8c9b0f0.jpg"
+  }
+]);
