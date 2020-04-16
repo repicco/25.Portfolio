b8adc825c7897cc6719344d180f6985f2c9073ea
Renato Picco <56326785+repicco@users.noreply.github.com>
2020-04-16 11:54:44 -0300

    Port.18
    Alteração Profile

--- portfolio/src/components/profile/profile.js
+++ portfolio/src/components/profile/profile.js
@@ -6,14 +6,15 @@ function Profile() {
     return (
         <div id='profile' className='container'>
                 <div className='card blue-grey lighten-4'>
+                    <div className='col s4 m2'>
+                        <img alt='Foto perfil' src={Foto} className='img_profile circle responsive-img'></img>
+                    </div>
                     <div className='row card-content'>
                         <div className='col s8 m4 offset-m3 right-align'>
                             <img alt='Logo' src={logo} className='img_logo responsive-img'></img>
                             <h5>Dev. Frontend</h5>
                         </div>
-                        <div className='col s4 m2'>
-                            <img alt='Foto perfil' src={Foto} className='img_profile circle responsive-img'></img>
-                        </div>
+
                     </div>
                 </div>
         </div>
