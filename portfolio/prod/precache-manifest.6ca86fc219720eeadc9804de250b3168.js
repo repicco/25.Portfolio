314df324181a2e06993232ba3793aca770af057d
Renato Picco <56326785+repicco@users.noreply.github.com>
2020-04-22 12:17:11 -0300

    Deploy.v27
    Correção de Imagens

---
+++ portfolio/prod/precache-manifest.6ca86fc219720eeadc9804de250b3168.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0f3cca7b5bd70c32b13b27fc5f4139",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdfa85fb4f2e948c525e",
+    "url": "/static/css/main.d40ccd9f.chunk.css"
+  },
+  {
+    "revision": "a8d4e3b098d32c193dbd",
+    "url": "/static/js/2.3cdd9729.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3cdd9729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdfa85fb4f2e948c525e",
+    "url": "/static/js/main.76d271eb.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "7be9144a64622c426cdf8b036d335127",
+    "url": "/static/media/Renato.7be9144a.jpg"
+  },
+  {
+    "revision": "f04235e7b7d5beb3050fd0645ba786b5",
+    "url": "/static/media/airbnb.f04235e7.jpg"
+  },
+  {
+    "revision": "2c228e2747bfbc00cd4a0128ed3b7b6c",
+    "url": "/static/media/atendi.2c228e27.jpg"
+  },
+  {
+    "revision": "93efee67242ae44766c44c9393258933",
+    "url": "/static/media/basico.93efee67.jpg"
+  },
+  {
+    "revision": "235cc582207eb8b43ebd7c48a6433ccb",
+    "url": "/static/media/biscoito.235cc582.jpg"
+  },
+  {
+    "revision": "8a238b39fd49e5128081356d71aaca59",
+    "url": "/static/media/bonus.8a238b39.jpg"
+  },
+  {
+    "revision": "77fa4863c08734898e93e1f1b2e6594a",
+    "url": "/static/media/churras.77fa4863.jpg"
+  },
+  {
+    "revision": "1a18e15dca02e75cdcee932304ead49d",
+    "url": "/static/media/edwcontabil.1a18e15d.jpg"
+  },
+  {
+    "revision": "28314f3c09f9277e96b3bee57f12e98c",
+    "url": "/static/media/financas.28314f3c.jpg"
+  },
+  {
+    "revision": "c99bdaa6fa8658629525b83ba23c2d3b",
+    "url": "/static/media/mercado.c99bdaa6.jpg"
+  },
+  {
+    "revision": "76350f273d00f985d39816217790d639",
+    "url": "/static/media/museu.76350f27.jpg"
+  },
+  {
+    "revision": "231e63e839333c4a680685e06fb31055",
+    "url": "/static/media/orcamento.231e63e8.jpg"
+  },
+  {
+    "revision": "c5739de5e37d71d1dfdd36d3c1572c70",
+    "url": "/static/media/parallax.c5739de5.jpg"
+  },
+  {
+    "revision": "ef0991e59f0176f7639b51dad3ac64c6",
+    "url": "/static/media/poupedinheiro.ef0991e5.jpg"
+  },
+  {
+    "revision": "9d9693b949a695ebf568f57dde57da7c",
+    "url": "/static/media/spotify.9d9693b9.jpg"
+  }
+]);
--- portfolio/prod/service-worker.js
+++ portfolio/prod/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bb053f1dac53a8583e2ef7082525a316.js"
+  "/precache-manifest.6ca86fc219720eeadc9804de250b3168.js"
 );
 
 self.addEventListener('message', (event) => {
